bffd20a38e65ff662f41b33718f5c41b73f3300a
Vishwaradhya <69788666+vmaradhya2020@users.noreply.github.com>
2024-10-02 17:16:27 +0530

    appLLAMA3.py

--- appLLAMA3.py
+++ appLLAMA3.py
@@ -10,3 +10,27 @@ def chatbot(prompt):
       ]
   )
   return response.choices[0].message.content
+
+# Streamlit app starts here
+def main():
+    st.title("GPT Chatbot Model - LLAMA3-8B-8192")
+    st.write("Enter your question below:")
+
+    # Input atext box for user
+    user_input = st.text_input("You:", "")
+
+    # Button to send input to chatbot
+    if st.button("Submit"):
+        if user_input:
+            # Call the chatbot function
+            response = chatbot(user_input)
+            st.write(f"Chatbot: {response}")
+        else:
+            st.write("Please enter a message.")
+
+# Set up your Groq API key
+groq_api_key = st.secrets["groq_api_key"] 
+
+# Run the main function
+if __name__ == "__main__":
+    main()
