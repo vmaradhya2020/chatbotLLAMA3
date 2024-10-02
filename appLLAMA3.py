d4c0f5f5da6b26e5bfe756f56ae95c0d2fa066dc
Vishwaradhya <69788666+vmaradhya2020@users.noreply.github.com>
2024-10-02 17:11:14 +0530

    appLLAMA3.py

--- appLLAMA3.py
+++ appLLAMA3.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import Groq
+from groq import Groq 
 
+def chatbot(prompt):
+  response = client.chat.completions.create(
+      model="LLAMA3-8B-8192",
+      messages=[
+          {"role": "user", "content": prompt}
+      ]
+  )
+  return response.choices[0].message.content
